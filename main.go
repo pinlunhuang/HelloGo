b8de4494471c91228eeb075a48339bdc2287e727
Pinlun <lunwaac@gmail.com>
2019-10-25 15:20:23 +0800

    Add SQL func

--- main.go
+++ main.go
@@ -2,29 +2,31 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func add(i, j int) int {
 	return i + j
 }
 
+func getUserListSQL(username, email string) string {
+	sql := "select * from user"
+	where := []string{}
+
+	if username != "" {
+		where = append(where, fmt.Sprintf("username = '%s'", username))
+	}
+
+	if email != "" {
+		where = append(where, fmt.Sprintf("email = '%s'", email))
+	}
+
+	return sql + " where " + strings.Join(where, " or ")
+}
+
 func main() {
-	fmt.Println(add(1, 2))
+	fmt.Println(getUserListSQL("lunwaac", ""))
 
-	foo := func() string {
-		return "Anonymous Func"
-	}
-
-	fmt.Println(foo())
-
-	bar := func() {
-		fmt.Println("Anonymous Func2")
-	}
-
-	bar()
-
-	go func(i, j int) { //Go routine
-		fmt.Println(i + j)
-	}(1, 2)
+	fmt.Println(getUserListSQL("lunwaac", "lunwaac@gmail.com"))
 
 }
